d08eb36622a0f218b8842a55ec8cd2b374be7564
tati-b127 <101515412+tati-b127@users.noreply.github.com>
2023-07-12 21:40:57 +0300

    burger fixed

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,5 +1,4 @@
 // Burger menu
-console.log(window.innerWidth);
 const burgerBtn = document.querySelector(".burger__btn");
 const burgerList = document.querySelector(".burger__list");
 const items = document.querySelectorAll(".burger__item");
@@ -10,15 +9,119 @@ const burgerItemLink = document.querySelectorAll(".burger__link");
 const burgerLastTrigger = document.querySelectorAll(
   ".burger__link-last-trigger"
 );
-console.log(burgerLastTrigger);
+console.log(window.innerWidth);
 if (window.innerWidth < 780) {
   console.log(window.innerWidth);
   setFlip();
+} else if (window.innerWidth > 780) {
+  burgerTrigger();
+  document.body.style.overflow = "scroll";
+}
+
+function burgerTrigger() {
+  burgerBtn.addEventListener("click", () => {
+    if (burgerList.classList.contains("hidden")) {
+      burgerList.classList.remove("hidden");
+      burgerList.classList.add("visible");
+      document.body.style.overflow = "hidden";
+    } else if (burgerList.classList.contains("visible")) {
+      burgerList.classList.remove("visible");
+      burgerList.classList.add("hidden");
+      const flips = document.querySelectorAll(".flip");
+      if (flips !== null) {
+        flips.forEach((flip) => {
+          flip.classList.remove("flip");
+        });
+      }
+      // if (dropdowns !== null) {
+      //   dropdowns.forEach((dropdown) => {
+      //     if (dropdown.classList.contains("visible")) {
+      //       dropdown.classList.remove("visible");
+      //     }
+      //   });
+      // }
+      document.body.style.overflow = "scroll";
+    }
+    const burgerLastList = document.querySelectorAll(".burger__list-last");
+    console.log(burgerLastList);
+    if (burgerLastList.classList.contains("visible")) {
+      burgerLastList.classList.remove("visible");
+    }
+  });
 }
 function setFlip() {
+  console.log(burgerLastTrigger);
+
+  // burgerList.classList.add("hidden");
+  burgerBtn.addEventListener("click", () => {
+    if (burgerList.classList.contains("hidden")) {
+      burgerList.classList.remove("hidden");
+      burgerList.classList.add("visible");
+      document.body.style.overflow = "hidden";
+    } else if (burgerList.classList.contains("visible")) {
+      burgerList.classList.remove("visible");
+      burgerList.classList.add("hidden");
+      const flips = document.querySelectorAll(".flip");
+      if (flips !== null) {
+        flips.forEach((flip) => {
+          flip.classList.remove("flip");
+        });
+      }
+      document.body.style.overflow = "scroll";
+    }
+    const burgerLastList = document.querySelectorAll(".burger__list-last");
+    console.log(burgerLastList);
+    if (burgerLastList.classList.contains("visible")) {
+      burgerLastList.classList.remove("visible");
+    }
+  });
+  burgerBack.forEach((e) => {
+    e.addEventListener("click", () => {
+      dropdowns.forEach((e) => {
+        if (e.classList.contains("visible")) {
+          e.classList.remove("visible");
+          e.classList.add("hidden");
+        }
+      });
+      items.forEach((item) => {
+        item.classList.remove("flip");
+      });
+      const flips = document.querySelectorAll(".flip");
+      flips.forEach((flip) => {
+        flip.classList.remove("flip");
+      });
+    });
+  });
+  burgerClose.forEach((e) => {
+    e.addEventListener("click", (e) => {
+      if (burgerList.classList.contains("visible")) {
+        dropdowns.forEach((e) => {
+          if (e.classList.contains("visible")) {
+            e.classList.remove("visible");
+            e.classList.add("hidden");
+          }
+        });
+        items.forEach((item) => {
+          item.classList.remove("flip");
+        });
+        const flips = document.querySelectorAll(".flip");
+        flips.forEach((flip) => {
+          flip.classList.remove("flip");
+        });
+
+        burgerList.classList.add("hidden");
+        burgerList.classList.remove("visible");
+        // if (window.innerWidth < 780) {
+        document.body.style.overflow = "scroll";
+        // }
+      }
+    });
+  });
+
+  let list;
   burgerItemLink.forEach((e) => {
     e.addEventListener("click", () => {
-      const list = e.nextSibling;
+      list = e.nextSibling;
       if (list !== null) {
         list.style.display = "block";
         items.forEach((item) => {
@@ -32,63 +135,27 @@ function setFlip() {
     e.addEventListener("click", () => {
       const lastList = e.nextSibling;
       console.log(lastList);
+      console.log(list);
       if (lastList !== null) {
         lastList.style.display = "block";
         items.forEach((item) => {
           item.classList.add("flip");
+        });
+        const dropItems = document.querySelectorAll(
+          // ".header__drop-item"
+          ".header__drop-item > .header__link"
+        );
+        // list.classList.add("hidden");
+        console.log(dropItems);
+        dropItems.forEach((e) => {
+          console.log(e);
+          e.classList.add("flip");
         });
         lastList.classList.add("visible");
       }
     });
   });
 }
-
-// burgerList.classList.add("hidden");
-burgerBtn.addEventListener("click", () => {
-  if (burgerList.classList.contains("hidden")) {
-    burgerList.classList.remove("hidden");
-    burgerList.classList.add("visible");
-    document.body.style.overflow = "hidden";
-  } else if (burgerList.classList.contains("visible")) {
-    burgerList.classList.remove("visible");
-    burgerList.classList.add("hidden");
-    document.body.style.overflow = "scroll";
-  }
-});
-burgerBack.forEach((e) => {
-  e.addEventListener("click", () => {
-    dropdowns.forEach((e) => {
-      if (e.classList.contains("visible")) {
-        e.classList.remove("visible");
-        e.classList.add("hidden");
-      }
-    });
-    items.forEach((item) => {
-      item.classList.remove("flip");
-    });
-  });
-});
-burgerClose.forEach((e) => {
-  e.addEventListener("click", (e) => {
-    if (burgerList.classList.contains("visible")) {
-      dropdowns.forEach((e) => {
-        if (e.classList.contains("visible")) {
-          e.classList.remove("visible");
-          e.classList.add("hidden");
-        }
-      });
-      items.forEach((item) => {
-        item.classList.remove("flip");
-      });
-
-      burgerList.classList.add("hidden");
-      burgerList.classList.remove("visible");
-      // if (window.innerWidth < 780) {
-      document.body.style.overflow = "scroll";
-      // }
-    }
-  });
-});
 
 // Tabs
 
